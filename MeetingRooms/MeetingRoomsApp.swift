089f99aea58da3e4302b2908cd522b91a358ffe0
Martin Mose Hansen <martinmosehansen@me.com>
2022-03-25 21:13:19 +0100

    Initial Commit

---
+++ MeetingRooms/MeetingRoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingRoomsApp.swift
+//  MeetingRooms
+//
+//  Created by Martin Mose Hansen on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MeetingRoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
